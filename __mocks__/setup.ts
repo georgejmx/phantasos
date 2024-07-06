542adeba2dca2a2827d93d66a4589435c2fc8beb
georgejmx <georgejm2358@protonmail.com>
2024-07-06 17:34:43 +0100

    chore: auto sign in for new users (#31)

--- __mocks__/setup.ts
+++ __mocks__/setup.ts
@@ -2,4 +2,8 @@ import { beforeAll, vi } from "vitest";
 
 beforeAll(() => {
     vi.mock("next/navigation", () => require("next-router-mock"));
+    vi.mock('next-auth/react', () => ({
+        signIn: vi.fn(),
+        signUp: vi.fn(),
+    }));
 });
