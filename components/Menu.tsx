83fd06b86ee3a0e2975d54eed610ed6d38fd327c
georgejmx <georgejm2358@protonmail.com>
2025-05-08 21:44:26 +0100

    chore: modernise account status

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,13 +1,12 @@
 import { MenuProps } from "../lib/types";
 import NavigationPane from "./NavigationPane";
 import ErrorBox from "./ErrorBox";
+import AccountBubble from "./AccountBubble";
 
 export default function Menu(props: MenuProps): JSX.Element {
     return (
         <>
-            {props.email && (
-                <p className="text-lime-600 p-2 text-center">You are signed in as {props.email}</p>
-            )}
+            {props.email && <AccountBubble email={props.email} />}
             {props.archetypes ? (
                 <NavigationPane dream={props.dream} archetypes={props.archetypes} />
             ) : (
